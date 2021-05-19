9a67cfbc2e1375206cf392e6f99578b3b4d1228e
Ping Chen <remotenonsense@gmail.com>
2021-05-19 15:36:00 +0900

    Import fake-indexeddb, patch tdweb and web-worker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import type { Platform } from '@textshq/platform-sdk'
+import indexedDB from 'fake-indexeddb'
+import IDBKeyRange from 'fake-indexeddb/lib/FDBKeyRange'
 import Worker from 'web-worker';
 global.Worker = Worker
+// @ts-expect-error
+global.window = global
+global.indexedDB = indexedDB
+global.IDBKeyRange = IDBKeyRange
 
 export default {
   get info() {
