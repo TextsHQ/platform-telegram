9459e356f6f6a9bfadeacb2b76fd2629e5d87dea
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-06-27 00:04:01 +0530

    fix Message.isSender

--- src/mappers.ts
+++ src/mappers.ts
@@ -329,8 +329,8 @@ export default class TelegramMapper {
   }
 
   mapMessage(msg: CustomMessage, readOutboxMaxId: number): Message {
-    const isThreadSender = msg instanceof Api.MessageService || !msg.senderId || msg.senderId.equals(msg.chatId)
-    const msgPeerId = msg.peerId ? getPeerId(msg.peerId) : undefined
+    const isSender = msg.senderId.equals(this.mapperData.me.id)
+    const isThreadSender = !isSender && (msg instanceof Api.MessageService || !msg.senderId || msg.senderId.equals(msg.chatId))
     const senderID = isThreadSender ? '$thread' : String(msg.senderId)
     const mapped: Message = {
       _original: stringifyCircular([msg, msg.media?.className, msg.action?.className]),
@@ -339,8 +339,7 @@ export default class TelegramMapper {
       editedTimestamp: msg.editDate && !msg.editHide ? new Date(msg.editDate * 1000) : undefined,
       forwardedCount: msg.forwards || (msg.forward ? 1 : 0),
       senderID,
-      isSender: (msg.out && !isThreadSender)
-        || (msgPeerId === this.mapperData.me.id?.toString() && !msg.fwdFrom), // if self thread but not forwarded
+      isSender,
       linkedMessageID: msg.replyToMsgId?.toString(),
       buttons: msg.replyMarkup && msg.chatId ? this.getMessageButtons(msg.replyMarkup, getMarkedId({ chatId: msg.chatId }), msg.id) : undefined,
       expiresInSeconds: msg.ttlPeriod,
