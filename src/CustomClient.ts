3dad0b1f6304e701b8a3cdc618ef27da1b4c33b5
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-09-13 18:59:09 -0700

    bump gramjs

--- src/CustomClient.ts
+++ src/CustomClient.ts
@@ -1,12 +1,11 @@
 import { setTimeout as sleep } from 'timers/promises'
 import { texts } from '@textshq/platform-sdk'
 import { Api, errors, TelegramClient } from 'telegram'
-import type { MTProtoSender } from 'telegram/network'
 
 export class CustomClient extends TelegramClient {
-  override async invoke<R extends Api.AnyRequest>(request: R, sender?: MTProtoSender): Promise<R['__response']> {
+  override async invoke<R extends Api.AnyRequest>(request: R, dcId?: number): Promise<R['__response']> {
     try {
-      const result = await super.invoke(request, sender)
+      const result = await super.invoke(request, dcId)
       return result
     } catch (err) {
       // https://github.com/gram-js/gramjs/blob/07e7e22b6d5294236479219930bde66290a0837a/gramjs/client/users.ts#L58
@@ -16,7 +15,7 @@ export class CustomClient extends TelegramClient {
         if (err.seconds <= 300) {
           texts.error(new Date().toLocaleString(), `Sleeping for ${err.seconds}s for ${request.className}`, request)
           await sleep((err.seconds * 1_000) + 1_000)
-          const result = await super.invoke(request, sender)
+          const result = await super.invoke(request, dcId)
           return result
         }
         texts.error(err, request)
