fc8fd24010a36cc6c8130426c5618bfddf040e6b
1Conan <me@1conan.com>
2024-03-04 20:21:16 +0900

    skip `ignore` on channels

--- src/api.ts
+++ src/api.ts
@@ -483,7 +483,11 @@ export default class TelegramAPI implements PlatformAPI {
       if (update.className === 'UpdatesTooLong') {
         await this.syncCommonState()
         ignore = true
-      } else if (TelegramAPI.NORMAL_UPDATES.includes(update.className)) {
+      } else if (TelegramAPI.SHORT_UPDATES.includes(update.className)) {
+        const regularUpdate = this.convertShortUpdate(update as Api.UpdateShort)
+        handleUpdate(regularUpdate)
+        return
+      } else if (TelegramAPI.NORMAL_UPDATES.includes(update.className) && !('channelId' in update)) {
         const { pts, ptsCount } = update as { pts: number, ptsCount: number }
         const sum = this.state.localState.pts + ptsCount
         if (sum === pts) {
@@ -499,10 +503,6 @@ export default class TelegramAPI implements PlatformAPI {
         }
 
         this.saveCommonState()
-      } else if (TelegramAPI.SHORT_UPDATES.includes(update.className)) {
-        const regularUpdate = this.convertShortUpdate(update as Api.UpdateShort)
-        handleUpdate(regularUpdate)
-        return
       }
 
       if (ignore) return
